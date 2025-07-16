48a0b6edf0016905665b1ad54859b2a7ba9019ac
Keishabit <keisha_bryant@yahoo.com>
2025-07-16 16:13:35 -0500

    update

---
+++ code-along-list.py
@@ -0,0 +1,40 @@
+# create an empty list
+
+todo_list = []
+print("Your todo list", todo_list)
+
+# append items to the list
+
+todo_list.append("Buy groceries")
+todo_list.append("Study")
+todo_list.append('Get gifts')
+
+print("Updated list:", todo_list)
+
+# insert an item 
+
+todo_list.insert(1, "Pay the bills")
+print("After inserting an imem:", todo_list)
+
+# Using indexes and slicing
+
+print("First task:", todo_list[0])
+print("Last two tasks:", todo_list[3:])
+
+# mark a task as done
+
+done_task = todo_list.pop(2)
+print("You completed:", done_task)
+print("Todo list after removal:",todo_list)
+
+# update a existing task
+
+print(todo_list[1])
+todo_list[1] = "Buy groceries and snacks"
+print("Updated todo list value:", todo_list)
+
+# print a task list
+
+print("Here's what you still need to do:")
+for task in todo_list:
+    print("-", task)
---
+++ exercise-for-loops.py
@@ -0,0 +1,44 @@
+# play with for loops
+
+# create a list
+
+# use for loop to iterate through the list
+
+goals_chart = []
+print("Your Weekly Goals", goals_chart)
+
+# append items to the list
+
+goals_chart.append("Start something new")
+goals_chart.append("Grow a plant")
+goals_chart.append('Get gifts')
+
+print("Your Weekly Goals:", goals_chart)
+
+# insert an item 
+
+goals_chart.insert(1, "Go for walk")
+print("After inserting an imem:", goals_chart)
+
+# Using indexes and slicing
+
+print("First task:", goals_chart[0])
+print("Last two tasks:", goals_chart[3:])
+
+# mark a task as done
+
+done_task = goals_chart.pop(2)
+print("You completed:", done_task)
+print("Goals after removal:",goals_chart)
+
+# update a existing task
+
+print(goals_chart[1])
+goals_chart[1] = "Be kind to self & others"
+print("Updated goals chart value:", goals_chart)
+
+# print a task list
+
+print("Here's what you still need to do:")
+for task in goals_chart:
+    print("-", task)
