3655ed919f9e8b80cc769b2d1d25d39240c268ab
Keishabit <keisha_bryant@yahoo.com>
2025-07-17 12:27:32 -0500

    updated exercise

--- code-along-flow-control.py
+++ code-along-flow-control.py
@@ -1,5 +1,35 @@
+'''
 our_number = 42
+
+is_guessed = False
+
+# the while loop runs until a condtion turns true
+while not is_guessed == False:
 
 # get input from the user
 
-guess = int(input("Enter your guess:"))
+    guess = int(input("Enter your guess:"))
+
+# conditional statements to check the guess
+
+if guess == our_number:
+    print('Hooray!')
+elif guess > our_number:
+    print('Too high!')
+else:
+    print('Too low!')
+'''
+
+counter = 1
+
+while counter < 2000:
+ 
+    if counter % 3 == 0 and counter % 5 == 0:
+        print(f'{counter}: Fizzbuzz')
+    elif counter % 3 == 0:
+        print(f'{counter}: Fizz')
+    elif counter % 5 == 0:
+        print(f'{counter}: Buzz')
+    counter+= 1 # This is a shortcut for counter = counter + 1
+else:
+    print('Done with while loop')
