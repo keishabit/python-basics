ae6b3e0ac3b490ca04183f3d722d5e36a8fd36e4
Keishabit <keisha_bryant@yahoo.com>
2025-07-18 15:15:15 -0500

    update

--- python-reference-playground.py
+++ python-reference-playground.py
@@ -1,4 +1,11 @@
-fruits = ['apple', 'orange', 'banana']
+## For loops- fruit loops
+
+fruits = ['apple 10', 'orange 15', 'banana 11']
 
 for fruit in fruits:
     print(fruit)
+
+##  Fruit Loops
+
+for i in range(len(fruits)):
+    print(f"count is {i} - Fruits is {fruits[i]}")
